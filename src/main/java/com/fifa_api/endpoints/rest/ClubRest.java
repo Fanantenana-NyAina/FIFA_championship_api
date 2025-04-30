724a0a932aeff972ef6274d076fd4ebcf0354ee2
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-04-30 23:11:19 +0300

    feat: add contructor, getter & setter

--- src/main/java/com/fifa_api/endpoints/rest/ClubRest.java
+++ src/main/java/com/fifa_api/endpoints/rest/ClubRest.java
@@ -1,5 +1,12 @@
 package com.fifa_api.endpoints.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class ClubRest {
     private String id;
     private String name;
