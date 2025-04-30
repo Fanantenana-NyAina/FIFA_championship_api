a0dc6deaee4653e831f4f5905e29331480386c9d
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-04-30 23:11:38 +0300

    fix: add constructor, getter & setter

--- src/main/java/com/fifa_api/endpoints/rest/CoachRest.java
+++ src/main/java/com/fifa_api/endpoints/rest/CoachRest.java
@@ -1,5 +1,12 @@
 package com.fifa_api.endpoints.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class CoachRest {
     private String name;
     private String nationality;
